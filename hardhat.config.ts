e03c68dd03fd3286c99183d91c1dd926341a6128
John <bioj@hotmail.fr>
2022-10-03 12:35:38 +0900

    Renew hardhat-project-template

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,14 @@
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-solhint');
-require('hardhat-docgen');
-require('hardhat-contract-sizer');
+import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-contract-sizer';
 
-require('dotenv').config();
+dotenv.config()
+
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
 const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.7',
     settings: {
@@ -39,11 +40,16 @@ module.exports = {
       url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
       accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
     },
-  },
-  docgen: {
-    path: './docs',
-    clear: true,
-    runOnCompile: false,
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+      gasPrice: 40000000000, // 40 gwei
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+      gasPrice: 40000000000, // 40 gwei
+    },
   },
   contractSizer: {
     alphaSort: true,
@@ -51,3 +57,5 @@ module.exports = {
     disambiguatePaths: true,
   },
 };
+
+export default config;
--- scripts/lib/deployed.ts
+++ scripts/lib/deployed.ts
@@ -1,36 +1,38 @@
 const { readFile, writeFile } = require('fs').promises;
 const chalk = require('chalk');
 
-const FILE_PATH = './deployed.json';
+const FILE_PATH = './helpers/deployed.json';
 /*
   This function should be called after a successuf deployment in your scripts.
   It will create/update a file named deployed.json containing deployment informations of your smart contracts.
   DO NOT EDIT deployed.json MANUALLY, this is an automatically generated file.
 */
-exports.deployed = async (contractName, networkName, address) => {
+exports.deployed = async (contractName: string, networkName: string, address: string): Promise<void> => {
   console.log(chalk.green.bold(`${contractName} deployed on ${networkName} at ${address}`));
   console.log(chalk.green(`updating ${FILE_PATH} with ${contractName} on ${networkName} at ${address}`));
   // Open and Read current FILE_PATH if exists
-  let jsonString = '';
-  let obj = {};
+  let jsonString: string = '';
+  let obj: {[key: string]: Object} = {};
   try {
     jsonString = await readFile(FILE_PATH, 'utf-8');
     obj = JSON.parse(jsonString);
   } catch (e) {
     // If does not exist, do nothing
   }
-  const _addrObj = {};
-  const _chainObj = {};
-  let _networksObj = {};
+  const _addrObj: {[key: string]: string} = {};
+  const _chainObj: {[key: string]: Object} = {};
+  let _networksObj: {[key: string]: Object} = {};
+
   _addrObj.address = address;
   _chainObj[networkName] = _addrObj;
   _networksObj = { ...obj[contractName], ..._chainObj };
   obj[contractName] = _networksObj;
+  
   jsonString = JSON.stringify(obj);
 
   try {
     await writeFile(FILE_PATH, jsonString);
-  } catch (e) {
+  } catch (e: any) {
     console.log(e.message);
     throw e;
   }
