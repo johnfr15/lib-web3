de217c34d8ecec96556a71ae5f1a675c2fbe5eaf
John <bioj@hotmail.fr>
2023-08-22 11:34:30 +0200

    typo

--- lib-gotchi/realm/alchemicaFacet.ts
+++ lib-gotchi/realm/alchemicaFacet.ts
@@ -10,7 +10,7 @@ dotenv.config()
 
 /*
   |***********************************|
-  |        alchemicaFacet INTERFACE            |
+  |        alchemicaFacet INTERFACE   |
   |___________________________________| 
 
   EVENT
