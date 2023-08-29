19af5ca526ff378b77628b863250aaaa41b523ea
John <bioj@hotmail.fr>
2023-08-29 19:46:27 +0200

    ✅ transfer evm

--- Orbiter/transfer/evm_transfer.ts
+++ Orbiter/transfer/evm_transfer.ts
@@ -1,7 +1,9 @@
 import { Contract, JsonRpcProvider, Wallet, ethers } from "ethers";
 import { CROSS_ADDRESS, CROSS_ADDRESS_ABI, ERC20_SOL_ABI, NETWORK_NAME_TO_ORBITERID } from "../config/constant";
 import { is_native_token } from "../utils/transfer"
-import { TxTransferArgs } from "../types";
+import { BridgeToken, TxTransferArgs } from "../types";
+import { encode_ext } from "../utils/transfer";
+import { TICKER } from "../config/constant";
 
 export const evm_transfer = ( txArgs: TxTransferArgs ) => {
     
@@ -11,10 +13,12 @@ export const evm_transfer = ( txArgs: TxTransferArgs ) => {
         transfer ( txArgs )
 }
 
-// eth (native token) address from/to starknet address
+// eth (native token) address to starknet address
 export const cross_transfer = async( txArgs: TxTransferArgs ) => {
 
-    const { evmSigner, network, fromChain, token, maker, amount } = txArgs
+    const { evmSigner, network, fromChain, token, maker, amount, crossAddressExt } = txArgs
+    const signer = new Wallet( evmSigner.privateKey, token.provider as JsonRpcProvider)
+    let tx, receipt;
 
     try {
 
@@ -23,18 +27,30 @@ export const cross_transfer = async( txArgs: TxTransferArgs ) => {
         if ( cross_address === '' )
             throw new Error(`cross transfer: Unknown cross address for network ${ txArgs.fromChain.name } need one for orbiter id: ${orbiter_id}` )
     
-        evmSigner.connect( token.provider as JsonRpcProvider )
-        const Cross_transfer = new Contract( cross_address, CROSS_ADDRESS_ABI, evmSigner )
+        const Cross_transfer = new Contract( cross_address, CROSS_ADDRESS_ABI, signer )
+        const extHex = encode_ext( crossAddressExt! ) // Concat the target network + wallet's address
+        const options = { value: "0x" + amount.toString(16) }
+
         
         if ( is_native_token( token.address ) )
         {
-            const tx = Cross_transfer.transfer()
-            console.log("Cross transfer ")
+            console.log("\nCross transfer...")
+
+            tx = await Cross_transfer.transfer( maker.makerAddress, extHex, options )
+            receipt = await tx.wait()
         }
         else // It is an erc20
         {
-    
+            await approve_erc20( cross_address, token, amount, signer )
+
+            console.log("\nCross transfer...")
+
+            tx = await Cross_transfer.transferERC20( token.address, maker.makerAddress, options.value, extHex )
+            receipt = await tx.wait()
         }
+
+        console.log("Transaction valided !")
+        console.log("Hash: ", tx.hash)
         
     } catch ( error ) {
         
@@ -47,7 +63,6 @@ export const transfer = async( txArgs: TxTransferArgs ) => {
 
     let tx, receipt;
     const { evmSigner, token, maker, amount } = txArgs
-    console.log( token.provider )
     const signer = new Wallet( evmSigner.privateKey, token.provider as JsonRpcProvider)
 
     try {
@@ -67,7 +82,7 @@ export const transfer = async( txArgs: TxTransferArgs ) => {
             receipt = await tx.wait()
         }
 
-        console.log("Transfer valided !")
+        console.log("\nTransfer valided !")
         console.log("hash: ", tx.hash)
         
     } catch ( error ) {
@@ -77,7 +92,24 @@ export const transfer = async( txArgs: TxTransferArgs ) => {
     }
 }
 
-export const approve_erc20 = ( txArgs: TxTransferArgs ) => {
+export const approve_erc20 = async( target: string, token: BridgeToken, amount: bigint, signer: Wallet ) => {
 
+    try {
+        
+        const erc20 = new Contract( token.address, ERC20_SOL_ABI, signer )
+    
+        console.log(`\nApproving ${target} to spend ${ ethers.formatUnits( amount, token.precision)} ${TICKER[token.address]}...`)
+    
+        // Approve amount + 10%
+        const tx = await erc20.approve( target, (amount * BigInt( 11 )) / BigInt( 10 ))
+        const receipt = await tx.wait()
+
+        console.log("\nTransaction valided !")
+        console.log("hash: ", tx.hash)
+
+    } catch (error) {
+     
+        throw error
+
+    }
 }
-
--- Orbiter/utils/transfer.ts
+++ Orbiter/utils/transfer.ts
@@ -1,3 +1,5 @@
+import { CrossAddressExt } from "../types"
+import { ethers } from "ethers"
 export const is_native_token = ( tokenAddress: string ): boolean => {
 
     // polygon matic token
@@ -5,4 +7,16 @@ export const is_native_token = ( tokenAddress: string ): boolean => {
       return true
 
     return /^0x0+$/i.test( tokenAddress )
+}
+
+/**
+ * 
+ * @param ext 
+ * @dev 
+ *  - This function will concatenate the target network ( 0x01 == polygon, 0x02 == starknet ) and the wallet address of the sender
+ *
+ */
+export const encode_ext = ( ext: CrossAddressExt ) => {
+
+  return ethers.concat( [ ext.type, ext.value ] )
 }
