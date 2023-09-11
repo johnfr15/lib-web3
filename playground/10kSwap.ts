ca01cf16e272ab053f088ec8707e28fd3775184d
John <bioj@hotmail.fr>
2023-09-11 11:25:32 +0200

    10kSwap: playground ready

--- playground/10kSwap.ts
+++ playground/10kSwap.ts
@@ -11,9 +11,10 @@ const main = async() => {
     
     try {
         // Set up
-        const network: 'TESTNET' | 'MAINNET' = "MAINNET" // Testnet | Mainnet
+        const network: 'TESTNET' | 'MAINNET' = "TESTNET" // Testnet | Mainnet
+        const provider = network === 'TESTNET' ? TESTNET_PROVIDER : MAINNET_PROVIDER
 
-        const starkSigner = new Account( MAINNET_PROVIDER, process.env.TEST_STARK_PUBLIC_KEY!, process.env.TEST_STARK_PRIVATE_KEY! )
+        const starkSigner = new Account( provider,  process.env.ACCOUNT_ADDRESS!, process.env.PRIVATE_KEY! )
 
         console.log("stark account: ", starkSigner.address)
         console.log("")
@@ -21,8 +22,8 @@ const main = async() => {
 
         // await l0kSwap.swap(
         //     starkSigner,
-        //     [ TOKENS[ network ].usdc, TOKENS[ network ].eth ],
-        //     "6.519125",
+        //     [ TOKENS[ network ].dai, TOKENS[ network ].eth ],
+        //     "0.01",
         //     null,
         //     network
         // )
@@ -30,15 +31,19 @@ const main = async() => {
         // l0kSwap.addLiquidity( 
         //     starkSigner,
         //     TOKENS[ network ].eth,
-        //     '0.000008571428571428',
-        //     TOKENS[ network ].dai,
         //     null,
+        //     TOKENS[ network ].dai,
+        //     "0.01",
+        //     false,
+        //     network
         // )
 
         // l0kSwap.withdrawLiquidity( 
         //     starkSigner,
         //     TOKENS[ network ].eth,
-        //     TOKENS[ network ].dai,       
+        //     TOKENS[ network ].dai,
+        //     100,
+        //     network      
         // )
 
 
