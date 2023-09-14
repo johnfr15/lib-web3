60f735b7fbd506a9c101c54eaaae7ad2097dd1fc
John <bioj@hotmail.fr>
2023-09-14 16:00:37 +0200

    add log balance

--- playground/mySwap.ts
+++ playground/mySwap.ts
@@ -1,6 +1,7 @@
 import { Account } from "starknet"
 import MySwap from "../Starknet/MySwap"
 import dotenv from "dotenv"
+import { log_balances } from "../Starknet/MySwap/log/logBalances"
 
 
 dotenv.config()
@@ -17,30 +18,29 @@ const main = async() => {
         const starkSigner = new Account( TESTNET_PROVIDER, process.env.ACCOUNT_ADDRESS!, process.env.PRIVATE_KEY! )
 
         console.log("stark account: ", starkSigner.address)
-        console.log("")
-
+        await log_balances( starkSigner, network )
 
         // await MySwap.swap(
         //     starkSigner,
-        //     [ TOKENS[ network ].dai, TOKENS[ network ].eth ],
-        //     "0.001",
+        //     [ TOKENS[ network ].eth, TOKENS[ network ].usdc ],
+        //     "0.003788862358341964",
         //     network
         // )
 
-        // MySwap.addLiquidity( 
+        // await MySwap.addLiquidity( 
         //     starkSigner,
-        //     TOKENS[ network ].dai,
-        //     "0.01",
-        //     TOKENS[ network ].eth,
+        //     TOKENS[ network ].usdc,
         //     null,
+        //     TOKENS[ network ].eth,
+        //     '0.001920459998875498',
         //     false,
         //     network
         // )
 
-        // MySwap.withdrawLiquidity( 
+        // await MySwap.withdrawLiquidity( 
         //     starkSigner,
+        //     TOKENS[ network ].usdc,
         //     TOKENS[ network ].eth,
-        //     TOKENS[ network ].dai,
         //     100,
         //     network      
         // )
