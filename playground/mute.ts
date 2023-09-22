55d68b89d12a02f8244746c6bfd127c82f1c1dee
John <bioj@hotmail.fr>
2023-09-22 22:47:39 +0200

    Mute: add fee check for swap

--- playground/mute.ts
+++ playground/mute.ts
@@ -1,8 +1,7 @@
-import { ethers, Wallet, Contract } from "ethers"
+import { Wallet } from "ethers"
 import Mute from "../ZkSync/Mute"
 import { log_balances } from "../ZkSync/Mute/log"
 import dotenv from "dotenv"
-import fs from "fs"
 
 dotenv.config()
 
@@ -14,11 +13,10 @@ const main = async() => {
     try {
         // Set up
         const network: 'TESTNET' | 'MAINNET' = "TESTNET" // Testnet | Mainnet
-        const USDC_TESTNET =  "0x0faF6df7054946141266420b43783387A78d82A9"
-        const DAI_TESTNET =  "0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b"
-        const signer = new Wallet( process.env.ETH_PRIVATE_KEY!, TESTNET_PROVIDER )
+        const USDC_TESTNET = "0x0faF6df7054946141266420b43783387A78d82A9"
+        const DAI_TESTNET = "0x3e7676937A7E96CFB7616f255b9AD9FF47363D4b"
 
-        
+        const signer = new Wallet( process.env.ETH_PRIVATE_KEY!, TESTNET_PROVIDER )
 
         console.log("account: ", signer.address)
         await log_balances( signer, network )
@@ -26,28 +24,28 @@ const main = async() => {
 
         await Mute.swap(
             signer,
-            [ ZERO_ADDRESS, TOKENS[ network ].dai ],
-            "0.001",
+            [ TOKENS[ network ].dai,  ZERO_ADDRESS ],
+            "40272.763067513332673864",
             network
         )
 
-        await  Mute.addLiquidity( 
-            signer,
-            TOKENS[ network ].eth,
-            null,
-            TOKENS[ network ].dai,
-            null,
-            true,
-            network
-        )
+        // await  Mute.addLiquidity( 
+        //     signer,
+        //     TOKENS[ network ].eth,
+        //     null,
+        //     TOKENS[ network ].dai,
+        //     null,
+        //     true,
+        //     network
+        // )
 
-        await Mute.withdrawLiquidity( 
-            signer,
-            TOKENS[ network ].eth,
-            TOKENS[ network ].dai,
-            100,
-            network
-        )
+        // await Mute.withdrawLiquidity( 
+        //     signer,
+        //     TOKENS[ network ].eth,
+        //     TOKENS[ network ].dai,
+        //     100,
+        //     network
+        // )
 
         // await Mute.swap(
         //     signer,
