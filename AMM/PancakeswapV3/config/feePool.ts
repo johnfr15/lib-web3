77c71355c2713a53176afec6137ee8a17dfdd71a
John <bioj@hotmail.fr>
2023-10-10 19:19:30 +0200

    AMM: update Best fee pools

--- AMM/PancakeswapV3/config/feePool.ts
+++ AMM/PancakeswapV3/config/feePool.ts
@@ -46,27 +46,27 @@ export const BEST_FEE_POOL: { [key in Chains]: any } = {
 
     bsc: {
 
-        BNB_WETH: Fees.MEDIUM,
-        BNB_USDC: Fees.MEDIUM,
-        BNB_USDT: Fees.LOW,
-        BNB_DAI: Fees.LOW,
+        WBNB_WETH: Fees.MEDIUM,
+        WBNB_USDC: Fees.MEDIUM,
+        WBNB_USDT: Fees.LOW,
+        WBNB_DAI: Fees.LOW,
 
-        WETH_BNB: Fees.MEDIUM,
+        WETH_WBNB: Fees.MEDIUM,
         WETH_USDC: Fees.LOW,
         WETH_USDT: Fees.LOW,
         WETH_DAI: Fees.LOW,
   
-        USDC_BNB: Fees.MEDIUM,
+        USDC_WBNB: Fees.MEDIUM,
         USDC_WETH: Fees.LOW,
         USDC_USDT: Fees.VERY_LOW,
         USDC_DAI: Fees.LOW,
   
-        USDT_BNB: Fees.LOW,
+        USDT_WBNB: Fees.LOW,
         USDT_WETH: Fees.LOW,
         USDT_USDC: Fees.VERY_LOW,
         USDT_DAI: Fees.VERY_LOW,
   
-        DAI_BNB: Fees.LOW,
+        DAI_WBNB: Fees.LOW,
         DAI_WETH: Fees.LOW,
         DAI_USDC: Fees.LOW,
         DAI_USDT: Fees.VERY_LOW,
