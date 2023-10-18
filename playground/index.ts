3d9b410071f73fd3d2d558254215c72a30105d7e
John <bioj@hotmail.fr>
2023-10-18 19:07:30 +0200

    test

--- playground/index.ts
+++ playground/index.ts
@@ -1,11 +1,16 @@
 import dotenv from "dotenv"
-import { ethers, Wallet } from "ethers"
+import { AddressLike, ethers, Wallet } from "ethers"
 import { Account } from "starknet"
+import starknet from "starknet"
 import UniswapV3 from "../AMM/UniswapV3"
 import SushiswapV3 from "../AMM/SushiSwapV3"
 import PancakeswapV3 from "../AMM/PancakeswapV3"
+import Mute from "../ZkSync/Mute"
+import l0kswap from "../Starknet/10kSwap"
 import Orbiter from "../Bridge/Orbiter"
 import Stargate from "../Bridge/Stargate"
+import { resolve_chain } from "../Bridge/Bungee/utils"
+import { Chains } from "../Bridge/Bungee/type/types"
 
 dotenv.config()
 
@@ -14,7 +19,7 @@ const route: any[] = [
     // POLYGON
     {
         chain: "polygon",
-        protocoles: UniswapV3,
+        protocols: UniswapV3,
         op: 'swap',
         args: {
             signer: undefined,
@@ -26,7 +31,7 @@ const route: any[] = [
     },
     {
         chain: "polygon",
-        protocoles: UniswapV3,
+        protocols: UniswapV3,
         op: 'addLiquidity',
         args: {
             signer: undefined,
@@ -40,7 +45,7 @@ const route: any[] = [
     },
     {
         chain: "polygon",
-        protocoles: UniswapV3,
+        protocols: UniswapV3,
         op: 'withdrawLiquidity',
         args: {
             signer: undefined,
@@ -50,82 +55,48 @@ const route: any[] = [
         }
     },
 
-    // BRIDGE POLYGON => ARBITRUM
+    
+
+
+
+
+
+
+
+
+    // BRIDGE POLYGON => BSC
     {
         chain: "polygon",
-        protocoles: Orbiter,
+        action: "bridge",
+        protocols: Orbiter,
         op: 'swap',
         args: {
             evmSigner: undefined,
             starkSigner: undefined,
             token: Orbiter.Constant.TOKENS["MAINNET"]["polygon"].usdc,
             fromChain: "polygon", 
-            toChain: "arbitrum",
-            
-            amount: "1",
-        }
-    },
-
-    // ARBITRUM
-    {
-        chain: "arbitrum",
-        protocoles: UniswapV3,
-        op: 'swap',
-        args: {
-            signer: undefined,
-            path: [ UniswapV3.Constant.TOKENS["arbitrum"].eth, UniswapV3.Constant.TOKENS["arbitrum"].usdc  ],
-            amountIn: null,
-            amountOut: "2",
-            chain: "arbitrum",
-        }
-    },
-    {
-        chain: "arbitrum",
-        protocoles: UniswapV3,
-        op: 'addLiquidity',
-        args: {
-            signer: undefined,
-            addressA: UniswapV3.Constant.TOKENS["arbitrum"].eth,                       
-            amountA: null,     
-            addressB: UniswapV3.Constant.TOKENS["arbitrum"].usdc,                       
-            amountB: null,     
-            chain: "arbitrum",
-            options: { max: true } 
-        }
-    },
-    {
-        chain: "arbitrum",
-        protocoles: UniswapV3,
-        op: 'withdrawLiquidity',
-        args: {
-            signer: undefined,
-            addressA: UniswapV3.Constant.TOKENS["arbitrum"].eth,                       
-            addressB: UniswapV3.Constant.TOKENS["arbitrum"].usdc,                       
-            chain: "arbitrum",
-        }
-    },
-
-
-    // BRIDGE ARBITRUM => BSC
-    {
-        chain: "arbitrum",
-        protocoles: Orbiter,
-        op: 'swap',
-        args: {
-            evmSigner: undefined,
-            starkSigner: undefined,
-            token: Orbiter.Constant.TOKENS["MAINNET"]["arbitrum"].usdc,
-            fromChain: "arbitrum", 
             toChain: "bsc",
             
-            amount: "1",
-        }
+            max: true,
+            network: 'MAINNET'
+        },
+        targetChain: "bsc",
+        targetToken: Orbiter.Constant.TOKENS["MAINNET"]["bsc"].usdc,
     },
+
+
+
+
+
+
+
+
+
 
     // BSC
     {
         chain: "bsc",
-        protocoles: PancakeswapV3,
+        protocols: PancakeswapV3,
         op: 'swap',
         args: {
             signer: undefined,
@@ -137,7 +108,7 @@ const route: any[] = [
     },
     {
         chain: "bsc",
-        protocoles: PancakeswapV3,
+        protocols: PancakeswapV3,
         op: 'addLiquidity',
         args: {
             signer: undefined,
@@ -151,7 +122,7 @@ const route: any[] = [
     },
     {
         chain: "bsc",
-        protocoles: PancakeswapV3,
+        protocols: PancakeswapV3,
         op: 'withdrawLiquidity',
         args: {
             signer: undefined,
@@ -162,10 +133,19 @@ const route: any[] = [
     },
 
 
+
+
+
+
+
+
+
+
     // BRIDGE BSC => AVALANCHE
     {
         chain: "bsc",
-        protocoles: Stargate,
+        action: "bridge",
+        protocols: Stargate,
         op: 'bridge',
         args: {
             signer: undefined,
@@ -173,14 +153,26 @@ const route: any[] = [
             tokenTo: Stargate.Constant.TOKENS["avalanche"].usdt,
             fromChain: "bsc", 
             toChain: "avalanche",
-            amount: "1",
-        }
+            amount: null,
+            options: { max: true }
+        },
+        targetChain: "avalanche",
+        targetToken: Stargate.Constant.TOKENS["avalanche"].usdt,
     },
+
+
+
+
+
+
+
+
+
 
     // AVALANCHE
     {
         chain: "avalanche",
-        protocoles: SushiswapV3,
+        protocols: SushiswapV3,
         op: 'swap',
         args: {
             signer: undefined,
@@ -192,7 +184,7 @@ const route: any[] = [
     },
     {
         chain: "avalanche",
-        protocoles: SushiswapV3,
+        protocols: SushiswapV3,
         op: 'addLiquidity',
         args: {
             signer: undefined,
@@ -206,13 +198,241 @@ const route: any[] = [
     },
     {
         chain: "avalanche",
-        protocoles: SushiswapV3,
+        protocols: SushiswapV3,
         op: 'withdrawLiquidity',
         args: {
             signer: undefined,
             addressA: SushiswapV3.Constant.TOKENS["avalanche"].avax,                       
             addressB: SushiswapV3.Constant.TOKENS["avalanche"].usdc,                       
             chain: "avalanche",
+        }
+    },
+
+
+
+
+
+
+
+
+
+
+
+    // BRIDGE AVALANCHE => ARBITRUM
+    {
+        chain: "avalanche",
+        action: "bridge",
+        protocols: Stargate,
+        op: 'bridge',
+        args: {
+            signer: undefined,
+            tokenFrom: Stargate.Constant.TOKENS["avalanche"].usdt,
+            tokenTo: Stargate.Constant.TOKENS["arbitrum"].usdc,
+            fromChain: "avalanche", 
+            toChain: "arbitrum",
+            amount: null,
+            options: { max: true }
+        },
+        targetChain: "arbitrum",
+        targetToken: Stargate.Constant.TOKENS["arbitrum"].usdc,
+    },
+
+
+
+
+
+
+
+
+
+
+    // ARBITRUM
+    {
+        chain: "arbitrum",
+        protocols: UniswapV3,
+        op: 'swap',
+        args: {
+            signer: undefined,
+            path: [ UniswapV3.Constant.TOKENS["arbitrum"].eth, UniswapV3.Constant.TOKENS["arbitrum"].usdc  ],
+            amountIn: null,
+            amountOut: "2",
+            chain: "arbitrum",
+        }
+    },
+    {
+        chain: "arbitrum",
+        protocols: UniswapV3,
+        op: 'addLiquidity',
+        args: {
+            signer: undefined,
+            addressA: UniswapV3.Constant.TOKENS["arbitrum"].eth,                       
+            amountA: null,     
+            addressB: UniswapV3.Constant.TOKENS["arbitrum"].usdc,                       
+            amountB: null,     
+            chain: "arbitrum",
+            options: { max: true } 
+        }
+    },
+    {
+        chain: "arbitrum",
+        protocols: UniswapV3,
+        op: 'withdrawLiquidity',
+        args: {
+            signer: undefined,
+            addressA: UniswapV3.Constant.TOKENS["arbitrum"].eth,                       
+            addressB: UniswapV3.Constant.TOKENS["arbitrum"].usdc,                       
+            chain: "arbitrum",
+        }
+    },
+
+
+
+
+
+
+
+
+
+    // BRIDGE ARBITRUM => ZKSYNC
+    {
+        chain: "arbitrum",
+        action: "bridge",
+        protocols: Orbiter,
+        op: 'swap',
+        args: {
+            evmSigner: undefined,
+            starkSigner: undefined,
+            token: Orbiter.Constant.TOKENS["MAINNET"]["arbitrum"].usdc,
+            fromChain: "arbitrum", 
+            toChain: "zksync",
+            
+            max: true,
+            network: 'MAINNET'
+        },
+        targetChain: "zksync",
+        targetToken: Orbiter.Constant.TOKENS["MAINNET"]["zksync"].usdc,
+    },
+
+
+
+
+
+
+
+
+
+    // ZKSYNC
+    {
+        chain: "zksync",
+        protocols: Mute,
+        op: 'swap',
+        args: {
+            signer: undefined,
+            path: [ Mute.Constant.TOKENS['MAINNET'].eth, Mute.Constant.TOKENS['MAINNET'].usdc  ],
+            amountIn: "0.0002",
+            network: 'MAINNET'
+        }
+    },
+    {
+        chain: "zksync",
+        protocols: Mute,
+        op: 'addLiquidity',
+        args: {
+            signer: undefined,
+            addressA: Mute.Constant.TOKENS['MAINNET'].eth,                       
+            amountA: null,     
+            addressB: Mute.Constant.TOKENS['MAINNET'].usdc,                       
+            amountB: null,     
+            max: true,
+            network: 'MAINNET'
+        }
+    },
+    {
+        chain: "zksync",
+        protocols: Mute,
+        op: 'withdrawLiquidity',
+        args: {
+            signer: undefined,
+            addressA: Mute.Constant.TOKENS['MAINNET'].eth,                       
+            addressB: Mute.Constant.TOKENS['MAINNET'].usdc,
+            percent: 100,
+            network: 'MAINNET'                 
+        }
+    },
+
+
+
+
+
+
+
+
+
+    // BRIDGE ZKSYNC => STARKNET
+    {
+        chain: "zksync",
+        action: "bridge",
+        protocols: Orbiter,
+        op: 'swap',
+        args: {
+            evmSigner: undefined,
+            starkSigner: undefined,
+            token: Orbiter.Constant.TOKENS["MAINNET"]["arbitrum"].usdc,
+            fromChain: "zksync", 
+            toChain: "starknet",
+            
+            max: true,
+            network: 'MAINNET'
+        },
+        targetChain: "starknet",
+        targetToken: Orbiter.Constant.TOKENS["MAINNET"]["starknet"].usdc,
+    },
+
+
+
+
+
+
+
+
+
+    // STARKNET
+    {
+        chain: "starknet",
+        protocols: l0kswap,
+        op: 'swap',
+        args: {
+            signer: undefined,
+            path: [ l0kswap.Constant.TOKENS['MAINNET'].eth, l0kswap.Constant.TOKENS['MAINNET'].usdc  ],
+            amountIn: null,
+            amountOut: "2",
+            network: 'MAINNET'
+        }
+    },
+    {
+        chain: "starknet",
+        protocols: l0kswap,
+        op: 'addLiquidity',
+        args: {
+            signer: undefined,
+            addressA: l0kswap.Constant.TOKENS['MAINNET'].eth,                       
+            amountA: null,     
+            addressB: l0kswap.Constant.TOKENS['MAINNET'].usdc,                       
+            amountB: null,     
+            max: true,
+            network: 'MAINNET'
+        }
+    },
+    {
+        chain: "starknet",
+        protocols: l0kswap,
+        op: 'withdrawLiquidity',
+        args: {
+            signer: undefined,
+            addressA: l0kswap.Constant.TOKENS['MAINNET'].eth,                       
+            addressB: l0kswap.Constant.TOKENS['MAINNET'].usdc,
+            percent: 100,
+            network: 'MAINNET'                 
         }
     },
 ]
@@ -222,20 +442,41 @@ const route: any[] = [
 
 const main = async() => {
 
-    const evmsigner = new Wallet( process.env.TEST_ETH_PRIVATE_KEY!, ethers.getDefaultProvider("mainnet") )
-    const starkSigner = new Account( Orbiter.Constant.MAINNET_PROVIDER, process.env.TEST_STARK_PUBLIC_KEY!, process.env.TEST_STARK_PRIVATE_KEY! )
+    let evmsigner = new Wallet( process.env.ETH_PRIVATE_KEY!, ethers.getDefaultProvider("mainnet") )
+    const starkSigner = new Account( Orbiter.Constant.STARKNET_TESTNET_PROVIDER, process.env.ACCOUNT_ADDRESS!, process.env.PRIVATE_KEY! )
+
+
+
+    // UniswapV3.Log.log_balances( evmsigner, "arbitrum" )
+
+    // listen_token( evmsigner, UniswapV3.Constant.TOKENS.arbitrum.usdc, 'arbitrum' )
+
+    // UniswapV3.swap( 
+    //     evmsigner, 
+    //     [ UniswapV3.Constant.TOKENS.arbitrum.eth, UniswapV3.Constant.TOKENS.arbitrum.usdc ],
+    //     '0.000001',
+    //     null,
+    //     "arbitrum"
+    // )
 
     try {
 
-        await route.forEach( async( task ) => {
-
-            const fn: any = task.protocoles[ task.op ]
+        await route.forEach( async( task ) => 
+        {
+            const fn: any = task.protocols[ task.op ]
             const args: any = task.args
+
+            
 
             // Populate arguments for function
             Object.entries( task.args ).forEach(( [key, value] ) => 
             {
-                if ( key === 'signer' || key == "evmSigner" ) args[ key ] = evmsigner
+                if ( key === 'signer' || key == "evmSigner" )
+                {
+                    evmsigner = resolve_chain( evmsigner, task.chain )
+                    args[ key ] = evmsigner
+                } 
+                if ( key === 'signer' && task.chain === 'starknet') args[ key ] = starkSigner
                 if ( key == "starkSigner" ) args.starkSigner = starkSigner
             })
 
@@ -245,13 +486,69 @@ const main = async() => {
             console.log( "\n\n" )
             // // Execute task
             // await fn( args )
+
+            if ( task.action === 'bridge' && task.targetChain !== 'starknet')
+                await listen_token( evmsigner, task.targetToken, task.targetChain )
         })
+
         
     } catch ( error ) {
         
         throw( error )
 
     }
+    
+
 }
+
+
+const listen_token = async(signer: Wallet, tokenAddress: AddressLike, chain: Chains) => {
+
+    signer = resolve_chain( signer, chain )
+    const tokenABI = ['event Transfer(address indexed from, address indexed to, uint256 value)', 'function decimals()'];
+
+    const tokenContract = new ethers.Contract( tokenAddress as string, tokenABI, signer );
+    const decimals: number = await tokenContract.decimals()
+
+    console.log(`Listening on ${ chain } for token: ${ tokenAddress }...`)
+    await tokenContract.once('Transfer', (from, to, value, event) => {
+
+        if ( to === signer.address ) {
+          console.log(`Received ${ ethers.formatUnits( value, decimals ) } from ${from} !`);
+          // Trigger your custom function here
+        }
+    });
+}
+
+const listen_starknet_token = async( signer: Account, tokenAddress: string ) => {
+
+    const provider = Orbiter.Constant.MAINNET_PROVIDER
+    let continuation_token = undefined
+    let currBlock = 0
+
+    while (true) 
+    {
+        const block = await provider.getBlock()
+
+        const events: any = await provider.getEvents({
+            from_block: "latest" as any,
+            to_block: "latest" as any,
+            chunk_size: 10,
+        })
+
+
+        if ( events.continuation_token !== continuation_token)
+        {
+            for( let event of events.events )
+            {
+                if ( event.fromAddress === tokenAddress.toLowerCase() )
+                    console.log( event )
+            }
+        }
+
+    
+    }
+}
+
 
 main()
